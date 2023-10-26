f6b6288d282c424d7b409d1821b7e412e87c3a6c
LuckyTain <8756469+LuckyTain@users.noreply.github.com>
2023-10-26 17:55:45 +0800

    Update banana_app.py

--- banana_app.py
+++ banana_app.py
@@ -1,4 +1,5 @@
 import base64
+import time
 
 from potassium import Potassium, Request, Response
 
@@ -12,10 +13,13 @@ app = Potassium("my_app")
 # @app.init runs at startup, and loads models into the app's context
 @app.init
 def init():
+    time1 = time.time()
     # vits_app.app.run(host='0.0.0.0', port=app.config.get("PORT", 23456), debug=app.config.get("DEBUG", False))  # 对外开放
     context = {
         "model": vits_app
     }
+    time2 = time.time()
+    print("init time:", time2 - time1)
     return context
     # vits_app.app.run(host='0.0.0.0', port=vits_app.app.config.get("PORT", 23456), debug=vits_app.app.config.get("DEBUG", False))
     # device = 0 if torch.cuda.is_available() else -1
@@ -31,6 +35,7 @@ def init():
 # @app.handler runs for every call
 @app.handler("/")
 def handler(context: dict, request: Request) -> Response:
+    time1 = time.time()
     text = request.json.get("text")
     speaker_id = request.json.get("id")
     task = {"text": text,
@@ -51,6 +56,8 @@ def handler(context: dict, request: Request) -> Response:
     file_base64 = base64.b64encode(file_byte).decode("utf-8")
     # cuda_status = torch.cuda.is_available()
     # print("cuda_status:", cuda_status)
+    time2 = time.time()
+    print("handler time:", time2 - time1)
 
     return Response(
         json={"outputs": "success", "file": file_base64},
